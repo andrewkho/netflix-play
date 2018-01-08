5f3015947adb2b98bb79d920a445e7321b8dd7dd
andrewkho <andrewkh@ece.ubc.ca>
2018-01-08 00:01:33 -0800

    Optimize the cython code a bit, add unroll a loop and try to parallelize

--- solvers/svd/svd_train_stochastic.pyx
+++ solvers/svd/svd_train_stochastic.pyx
@@ -3,7 +3,10 @@
 #cython: cdivision=True
 #cython: nonecheck=False
 
-from libc.math cimport abs
+from libc.math cimport fabs
+from libc.stdio cimport printf
+
+from cython.parallel import prange
 
 import numpy as np
 cimport numpy as np
@@ -15,11 +18,11 @@ def svd_train_sgd(double[:,:] left, double[:,:] right, float[:] resid, int[:] ra
 
 cdef float _svd_train_sgd(double[:,:] left, double[:,:] right, float[:] resid, int[:] rand_order,
                              int[:] uids, int[:] mids, double[:] ratings, double rate, double eps,
-                             int maxiters, double gamma, int include_bias):
+                             int maxiters, double gamma, int include_bias) nogil:
 
     cdef double max_change, dleft, dright, dresid
     cdef double rat, yhat, err, total_err, old_resid
-    cdef int uid, mid, ob, iter, k
+    cdef int uid, mid, ob, iter, k_, k, _
 
     old_resid = 0
     max_change = eps + 1
@@ -33,34 +36,46 @@ cdef float _svd_train_sgd(double[:,:] left, double[:,:] right, float[:] resid, i
             rat = ratings[ob]
 
             yhat = 0
-            for k_ in range(left.shape[1]):
+            for k_ in prange(left.shape[1]):
                 yhat += left[uid, k_] * right[mid, k_]
             err = rat - yhat
             total_err += err*err
 
-            for k in range(left.shape[1]):
-                dleft = err * right[mid, k] - gamma * left[uid, k]  ## gamma controls regularization
+            if include_bias:
+                k = 0
                 dright = err * left[uid, k] - gamma * right[mid, k]
-                if include_bias and k == 0:
-                    dleft = 0
-                if include_bias and k == 1:
-                    dright = 0
-                left[uid, k] += rate * dleft
                 right[mid, k] += rate * dright
 
-                if rate*rate*dleft*dleft > max_change:
-                    max_change = rate*rate*dleft*dleft
-                if rate*rate*dright*dright > max_change:
-                    max_change = rate*rate*dright*dright
+                k = 1
+                dleft = err * right[mid, k] - gamma * left[uid, k]  ## gamma controls regularization
+                left[uid, k] += rate * dleft
+
+                for k in prange(2, left.shape[1]):
+                    dleft = err * right[mid, k] - gamma * left[uid, k]  ## gamma controls regularization
+                    dright = err * left[uid, k] - gamma * right[mid, k]
+                    left[uid, k] += rate * dleft
+                    right[mid, k] += rate * dright
+
+            else:
+                for k in prange(left.shape[1]):
+                    dleft = err * right[mid, k] - gamma * left[uid, k]  ## gamma controls regularization
+                    dright = err * left[uid, k] - gamma * right[mid, k]
+                    left[uid, k] += rate * dleft
+                    right[mid, k] += rate * dright
+
+                    # if rate*rate*dleft*dleft > max_change:
+                    #     max_change = rate*rate*dleft*dleft
+                    # if rate*rate*dright*dright > max_change:
+                    #     max_change = rate*rate*dright*dright
 
         if iter % 20 == 0:
-            print "  max_change %e, eps: %e, resid: %e" % (max_change, eps, total_err)
-        if abs(old_resid - total_err)/total_err < eps:
-            break
-        old_resid = total_err
-        if iter == maxiters:
-            print "  Reached max iters!!!"
+            printf("  resid: %e\n", total_err)
+            if fabs(old_resid - total_err)/total_err < eps:
+                break
+            old_resid = total_err
 
+        if iter == maxiters:
+            printf("  Reached max iters!!!\n")
     return iter
 
 
